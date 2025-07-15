bd83a54ca99ff2973ea86cc698f06ca2d360741f
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-14 21:14:54 -0300

    Adicionando classe Ingresso do exercício 1.

---
+++ src/br/com/guilherme/tesch/models/Ingresso.java
@@ -0,0 +1,32 @@
+package br.com.guilherme.tesch.models;
+
+public class Ingresso {
+    private double valor;
+    private String nameFilm;
+    private String typeAudio;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getNameFilm() {
+        return nameFilm;
+    }
+
+    public void setNameFilm(String nameFilm) {
+        this.nameFilm = nameFilm;
+    }
+
+    public String getTypeAudio() {
+        return typeAudio;
+    }
+
+    public void setTypeAudio(String typeAudio) {
+        this.typeAudio = typeAudio;
+    }
+
+}
