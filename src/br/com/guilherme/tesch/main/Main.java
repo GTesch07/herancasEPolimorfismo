3109c9cc7116bb43bfe5df948a3a09d7d4f72883
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-16 20:45:32 -0300

    Modifying the Salesman class.

--- src/br/com/guilherme/tesch/main/Main.java
+++ src/br/com/guilherme/tesch/main/Main.java
@@ -9,13 +9,11 @@ public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         Maneger maneger = new Maneger("Guilherme da Silva Tesch", "guilherme_tesch", "21312313", true, 24000.00);
-        Salesman salesman = new Salesman();
-
-        salesman.setName("Mario");
-        salesman.setCode("12342345");
+        Salesman salesman = new Salesman("Monica Tesch", "monica@tesch.com", "123456", 8);
 
         maneger.imprimirDetalhes();
-
+        System.out.println("");
+        salesman.imprimirDetalhes();
         scanner.close();
     }
 }
--- src/br/com/guilherme/tesch/models/Salesman.java
+++ src/br/com/guilherme/tesch/models/Salesman.java
@@ -2,6 +2,18 @@ package br.com.guilherme.tesch.models;
 
 public non-sealed class Salesman extends Employee {
     private double percentPerSold;
+    private int quantitySold;
+
+    public Salesman(String name, String email, String password, int quantitySold) {
+        super.setName(name);
+        super.setEmail(email);
+        super.setPassword(password);
+        this.quantitySold = quantitySold;
+    }
+
+    public int getQuantitySold() {
+        return quantitySold;
+    }
 
     public double getPercentPerSold() {
         return percentPerSold;
@@ -9,5 +21,12 @@ public non-sealed class Salesman extends Employee {
 
     public void setPercentPerSold(double percentPerSold) {
         this.percentPerSold = percentPerSold;
+    }
+
+    public void imprimirDetalhes() {
+        System.out.println("Name: " + super.getName());
+        System.out.println("Email: " + super.getEmail());
+        System.out.println("Password: " + super.getPassword());
+        System.out.println("Quantity Sold: " + quantitySold);
     }
 }
